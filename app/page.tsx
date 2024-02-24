90e233aa38b404785d86bb9689fdc492bbdaf982
AleAndino <aleandino.cbaa@gmail.com>
2024-02-24 19:11:38 -0300

    Booking

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import Booking from "@/components/Booking/Booking";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Hello
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div>
+          <Booking />
+        </div>
+        <div className="col-span-2">
+          Map
+        </div>
+      </div>
     </div>
   );
 }
